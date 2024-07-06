21d9fe28ae2f437f26aede1f5a3bba814d9eae90
Nina-Dubrovina <alex_ist27@mail.ru>
2024-07-06 10:21:35 +0300

    1 версия 1 тест расчета факториала junit5

--- src/test/java/FactorialTest.java
+++ src/test/java/FactorialTest.java
@@ -8,4 +8,19 @@ public class FactorialTest {
     public void factorialZeroTest() {
        assertEquals(1, Factorial.calculateFactorial(0));
     }
+    @Test
+    public void factorialOneTest() {
+        assertEquals (1, Factorial.calculateFactorial(1));
+
+    }
+    @Test
+    public void factorialFiveTest() {
+        assertEquals (120, Factorial.calculateFactorial(5));
+
+    }
+    @Test
+    public void factorialNegativTest() {
+        assertThrows (IllegalArgumentException.class, ()-> Factorial.calculateFactorial(-1));
+
+    }
 }
